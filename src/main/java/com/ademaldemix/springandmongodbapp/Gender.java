6dea7a8ddad6a724ee789aba0c61214731d3bc65
Adem Aldemir <adem.aldemir@outlook.com>
2022-06-10 16:30:32 +0300

    Initial commit

---
+++ src/main/java/com/ademaldemix/springandmongodbapp/Gender.java
@@ -0,0 +1,2 @@
+package com.ademaldemix.springandmongodbapp;public enum Gender {
+}
---
+++ src/main/java/com/ademaldemix/springandmongodbapp/SpringAndMongodbApplication.java
@@ -0,0 +1,10 @@
+package com.ademaldemix.springandmongodbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndMongodbApplication.class, args
